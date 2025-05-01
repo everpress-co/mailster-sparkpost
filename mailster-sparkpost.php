c4b0418d62f1467a78e98034b3539dbb13bbfab1
Xaver <xaver@everpress.co>
2025-05-01 16:02:10 +0200

    updated version

--- mailster-sparkpost.php
+++ mailster-sparkpost.php
@@ -4,7 +4,7 @@ Plugin Name: Mailster SparkPost Integration
 Requires Plugins: mailster
 Plugin URI: https://mailster.co/?utm_campaign=wporg&utm_source=wordpress.org&utm_medium=plugin&utm_term=SparkPost
 Description: Uses SparkPost to deliver emails for the Mailster Newsletter Plugin for WordPress.
-Version: 1.8.1
+Version: 1.9.0
 Author: EverPress
 Author URI: https://mailster.co
 Text Domain: mailster-sparkpost
@@ -12,7 +12,7 @@ License: GPLv2 or later
 */
 
 
-define( 'MAILSTER_SPARKPOST_VERSION', '1.8.1' );
+define( 'MAILSTER_SPARKPOST_VERSION', '1.9.0' );
 define( 'MAILSTER_SPARKPOST_REQUIRED_VERSION', '4.0' );
 define( 'MAILSTER_SPARKPOST_FILE', __FILE__ );
 
